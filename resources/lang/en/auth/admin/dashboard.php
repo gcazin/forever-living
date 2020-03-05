dbc790c62fc0887988c519dd1f2745076e7c7cc8
gcazin <czn.guillaume@gmail.com>
2020-03-05 15:32:09 +0100

    feat: Dashboard Admin

---
+++ resources/lang/en/auth/admin/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Welcome to our admin dashboard',
+    'members' => 'Numbers of FBO members',
+    'users' => 'Users',
+    'user' => 'User',
+    'fbo_number' => 'FBO Number',
+    'passcode' => 'Passcode',
+    'email' => 'Email address',
+    'role' => 'Role',
+    'actions' => 'Actions',
+    'edit' => 'Edit'
+];
---
+++ resources/lang/fr/auth/admin/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Bienvenue dans votre espace admin',
+    'members' => 'Nombres de membres FBO',
+    'users' => 'Utilisateurs',
+    'user' => 'Utilisateur',
+    'fbo_number' => ' Numéro FBO',
+    'passcode' => 'Passcode',
+    'email' => 'Adresse email',
+    'role' => 'Rôle',
+    'actions' => 'Actions',
+    'edit' => 'Modifier'
+];
