653fa7fb49adba628a7c1fd8749cfad70278b490
gcazin <czn.guillaume@gmail.com>
2020-03-20 14:18:24 +0100

    ref: Couleur des liens actifs

--- resources/views/auth/admin/partials/menu.blade.php
+++ resources/views/auth/admin/partials/menu.blade.php
@@ -1,12 +1,12 @@
 <div class="menu my-3 px-2 flex flex-col">
     <span class="text-gray-600 text-sm px-4 mt-3 mb-2">Navigation</span>
-    <a href="{{ route('dashboard.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all">
+    <a href="{{ route('dashboard.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all {{ (request()->is('dashboard/admin')) ? 'bg-gray-800' : '' }}">
         <i class="fas fa-home text-lg mx-2"></i>
         <span class="font-bold">{{ $title_section ?? "Dashboard" }}</span>
     </a>
 
     <span class="text-gray-600 text-sm px-4 mt-3 mb-2">Gestion des utilisateurs</span>
-    <a href="{{ route('dashboard.admin.notifications') }}" class="relative hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all">
+    <a href="{{ route('dashboard.admin.notifications') }}" class="relative hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all {{ (request()->is('dashboard/admin/notifications')) ? 'bg-gray-800' : '' }}">
         <i class="fas fa-tasks text-lg mx-2"></i>
         <span class="font-bold">Traiter les demandes</span>
         @if(count(\App\Notification::all()->where('read_at', '===', NULL)) > 0)
@@ -15,11 +15,11 @@
     </a>
 
     <span class="text-gray-600 text-sm px-4 mt-3 mb-2">Gérér le contenu</span>
-    <a href="{{ route('manage.content.index.formation.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all">
+    <a href="{{ route('manage.content.index.formation.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all {{ (request()->is('dashboard/admin/contenu/formations')) ? 'bg-gray-800' : '' }}">
         <i class="fas fa-briefcase text-lg mx-2"></i>
         <span class="font-bold">Formations</span>
     </a>
-    <a href="{{ route('manage.content.index.home.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all">
+    <a href="{{ route('manage.content.index.home.admin') }}" class="hover:bg-gray-800 text-gray-200 hover:text-white px-2 py-2 mb-1 rounded-lg duration-200 transition-all {{ (request()->is('dashboard/admin/contenu/accueil')) ? 'bg-gray-800' : '' }}">
         <i class="fas fa-photo-video text-lg mx-2"></i>
         <span class="font-bold">Accueil</span>
     </a>
